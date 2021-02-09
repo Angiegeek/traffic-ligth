7b9dee4a056f2690e2a923bd67f4270b98be5566
Angiegeek <avm03021993@gmail.com>
2021-02-09 00:05:34 +0000

    terminado el trabajo de semaforo con luces que brillan con la funcion onClick

--- src/js/component/trafficligth.js
+++ src/js/component/trafficligth.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class TrafficLight extends React.Component {
-	render() {
-		return (
-			<div id="traffictop">
-				<div className="stickArea">
-					<div className="vl"></div>
-				</div>
-				<div className="container">
-					<div className="red light" />
-					<div className="yellow light" />
-					<div className="green light" />
-				</div>
+export const TrafficLight = () => {
+	//    variable de estado - modificador
+	const [color, setColor] = useState("red");
+	return (
+		<div id="traffictop">
+			<div className="stickArea">
+				<div className="vl"></div>
 			</div>
-		);
-	}
-}
+			<div className="container">
+				<div
+					className={"red light " + (color == "red" ? "active" : "")}
+					onClick={() => {
+						setColor("red");
+					}}
+				/>
+				<div
+					className={
+						"yellow light " + (color == "yellow" ? "active" : "")
+					}
+					onClick={() => {
+						setColor("yellow");
+					}}
+				/>
+				<div
+					className={
+						"green light " + (color == "green" ? "active" : "")
+					}
+					onClick={() => {
+						setColor("green");
+					}}
+				/>
+			</div>
+		</div>
+	);
+};
